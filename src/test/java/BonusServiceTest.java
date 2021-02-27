d49981e7667604dfdd4b51fbff85ed6e1821dbdd
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-02-27 18:50:38 +0300

    Добавление тестов

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -22,7 +22,30 @@ class BonusServiceTest {
         // подготавливаем данные:
         long amount = 20_000_60;
         boolean registered = true;
-        long expected = 300;
+        long expected = 500;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldCalculateNotRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 20_000_60;
+        boolean registered = false;
+        long expected = 200;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldCalculateNotRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
         assertEquals(expected,actual);
